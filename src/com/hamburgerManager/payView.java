40f6363a48c3d5a73910773f6df171c494648ef1
jeonghee <hongi_@naver.com>
2024-01-11 20:42:29 +0900

    이모지 완성 + 가독성 향상

--- src/com/hamburgerManager/payView.java
+++ src/com/hamburgerManager/payView.java
@@ -5,23 +5,25 @@ import static com.hamburgerManager.Pay.price;
 public class payView {
     public void singleMemberPay(){
         int coupon =(int) (Math.random()*6 + 5);
-        System.out.println("⭐ 1주년 오픈 이벤트로 5%~10% 회원쿠폰 적용되었습니다. ⭐");
-        System.out.println("이벤트 쿠폰" + coupon + " % 쿠폰 할인 적용되어 총 금액은 " + ( price /100 *(100-coupon))  + "원 입니다.");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("⭐ 1주년 오픈 이벤트로 5% ~ 10% 회원 전용 랜덤 쿠폰 적용되었습니다. ⭐");
+        System.out.println("이벤트 쿠폰 " + coupon + " % 쿠폰 할인 적용되어 총 금액은 " + ( price /100 *(100-coupon))  + "원 입니다.");
         price=0;
         System.out.println("최종 결제 완료되었습니다. 👌😁");
-        System.out.println("-------------------------");
+        System.out.println("--------------------------------------------------------------------------");
     }
 
     public void setMemberPay(){
         int coupon =(int) (Math.random()*6 + 5);
         int setFinalPay = ( price /100 *(100-coupon));
-        System.out.println("⭐ 1주년 오픈 이벤트로 5%~10% 회원쿠폰 적용되었습니다. ⭐");
-        System.out.println("이벤트 쿠폰" + coupon + " % 쿠폰 할인 적용되어 " + setFinalPay + " 원 입니다.");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("⭐ 1주년 오픈 이벤트로 5% ~ 10% 회원 전용 랜덤 쿠폰 적용되었습니다. ⭐");
+        System.out.println("이벤트 쿠폰 " + coupon + " % 쿠폰 할인 적용되어 " + setFinalPay + " 원 입니다.");
         System.out.println("🙌 세트 할인 1000원이 적용되었습니다. 🙌");
         System.out.println("총 금액은 " + (setFinalPay-1000)  + "원 입니다.");
         price=0;
         System.out.println("최종 결제 완료되었습니다. 👌😁");
-        System.out.println("-------------------------");
+        System.out.println("--------------------------------------------------------------------------");
     }
 
 }
